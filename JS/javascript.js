9870e1e15a47952a59204381c01a4cfbeb5b6c10
Eggi98 <edgarmiguel98@gmail.com>
2021-09-21 19:05:28 -0500

    primer commit

---
+++ JS/javascript.js
@@ -0,0 +1,12 @@
+alert("Los adultos son muy raros, si");
+
+
+
+
+
+confirm("¿Quieres salir de esta pagina?");
+
+prompt("¿Cual es tu color favorito?");
+
+console.log("Una historia tan simple y tan hermosa");
+
